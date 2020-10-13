ed63f67b17d0b779c8ed872a42915fe8bb483683
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-13 20:05:56 +0700

    Implement redux and theming in app

--- next-atom-with-auth/src/pages/_app.js
+++ next-atom-with-auth/src/pages/_app.js
@@ -1,7 +1,38 @@
-import "../styles/globals.css";
+import Router from "next/router";
+import NProgress from "nprogress";
+import PropTypes from "prop-types";
+import { ThemeProvider } from "styled-components";
+
+import theme from "styles/theme";
+
+import "normalize.css";
+import "styles/nprogress.css";
+import "styles/antd.custom.less";
+import "styles/globals.css";
+
+import { wrapper } from "stores";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+Router.events.on("routeChangeError", () => {
+  NProgress.done();
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp;
+MyApp.propTypes = {
+  Component: PropTypes.any,
+  pageProps: PropTypes.object
+};
+
+export default wrapper.withRedux(MyApp);
