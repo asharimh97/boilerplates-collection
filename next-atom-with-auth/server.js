a6910c65b14dbf8d6576de88208b6df7badcbb32
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-13 09:06:17 +0700

    Add express server config

---
+++ next-atom-with-auth/server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const session = require("express-session");
+const next = require("next");
+const dotenv = require("dotenv");
+
+const dev = process.env.NODE_ENV !== "production";
+// set target to folder src
+const app = next({ dir: "./src", dev });
+
+// routing
+const appRoutes = require("./src/routes");
+
+const handler = appRoutes.getRequestHandler(app);
+
+// logger
+const logger = require("./src/services/logger");
+
+dotenv.config();
+
+const { APP_ORIGIN, APP_PORT: PORT } = process.env;
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    server.use(
+      session({
+        name: "next-atom-boilerplate",
+        secret: "hii saru saru o",
+        resave: true,
+        saveUninitialized: true,
+        cookie: {
+          httpOnly: true,
+          maxAge: 7 * 24 * 3600 * 1000
+        }
+      })
+    );
+
+    appRoutes(server);
+
+    server.use("/static/", express.static("public"));
+    server.use("/public/", express.static("public"));
+
+    server.use(handler).listen(PORT, err => {
+      if (err) throw err;
+
+      logger.info(`> Ready on ${ORIGIN}`);
+    });
+  })
+  .catch(error => {
+    logger.error(error.stack);
+    process.exit(1);
+  });
