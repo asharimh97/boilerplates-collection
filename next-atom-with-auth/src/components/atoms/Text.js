8fe7e8a3568e3efe67e5442dd2658bec0cafa684
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-16 20:27:11 +0700

    Add text and title component

---
+++ next-atom-with-auth/src/components/atoms/Text.js
@@ -0,0 +1,75 @@
+import styled, { css } from "styled-components";
+import { color, space, typography } from "styled-system";
+import propTypes from "@styled-system/prop-types";
+import themeGet from "@styled-system/theme-get";
+
+const bold = css`
+  font-weight: ${props => props.bold && props.theme.bold};
+`;
+
+const semibold = css`
+  font-weight: ${props => props.semibold && props.theme.semibold};
+`;
+
+const medium = css`
+  font-weight: ${props => props.medium && props.theme.medium};
+`;
+
+const caps = css`
+  text-transform: ${props => props.caps && "capitalize"};
+`;
+
+const uppercase = css`
+  text-transform: ${props => props.uppercase && "uppercase"};
+`;
+
+const lowercase = css`
+  text-transform: ${props => props.lowercase && "lowercase"};
+`;
+
+const truncate = css`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  word-break: break-word;
+`;
+
+const small = css`
+  font-size: ${props => props.small && props.theme.fontSizes[0]};
+`;
+
+const Text = styled.p`
+  color: ${themeGet("colors.textColor.primary")};
+  ${typography}
+  ${space}
+  ${color}
+
+  ${small};
+
+  ${bold};
+  ${semibold};
+  ${medium};
+
+  ${caps};
+  ${uppercase};
+  ${lowercase};
+
+  ${props => props.truncate && truncate};
+`;
+
+Text.propTypes = {
+  ...propTypes.color,
+  ...propTypes.space,
+  ...propTypes.typography
+};
+
+Text.displayName = "Text";
+
+Text.defaultProps = {
+  fontSize: 1,
+  lineHeight: "1.65",
+  mx: 0,
+  my: 0
+};
+
+export default Text;
---
+++ next-atom-with-auth/src/components/atoms/Title.js
@@ -0,0 +1,30 @@
+import styled, { css } from "styled-components";
+import theme from "styles/theme";
+import Text from "./Text";
+
+const titleStyles = {
+  h1: { fontSize: theme.fontSizes[6] },
+  h2: { fontSize: theme.fontSizes[5] },
+  h3: { fontSize: theme.fontSizes[4] },
+  h4: { fontSize: theme.fontSizes[3] },
+  h5: { fontSize: theme.fontSizes[2] }
+};
+
+const Title = styled(Text)`
+  ${props =>
+    props.as &&
+    css`
+      ${titleStyles[props.as]};
+    `}
+`;
+
+Title.displayName = "Title";
+
+Title.defaultProps = {
+  as: "h1",
+  lineHeight: "1.15",
+  mb: "1.05rem",
+  mt: "2rem"
+};
+
+export default Title;
