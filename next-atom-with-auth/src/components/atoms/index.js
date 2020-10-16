6b84a1c66cbb0244ef2baaa7f772c5b3167d2d97
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-16 20:27:31 +0700

    Index components

--- next-atom-with-auth/src/components/atoms/index.js
+++ next-atom-with-auth/src/components/atoms/index.js
@@ -1,6 +1,10 @@
 export { default as Absolute } from "./Absolute";
 export { default as Box } from "./Box";
+export { default as Card } from "./Card";
+export { default as Container } from "./Container";
 export { default as Fixed } from "./Fixed";
 export { default as Flex } from "./Flex";
 export { default as Grid } from "./Grid";
 export { default as Sticky } from "./Sticky";
+export { default as Text } from "./Text";
+export { default as Title } from "./Title";
