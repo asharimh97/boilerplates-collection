8770535ed3c5043076f97c38ff2ea4775e5eb1f6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-20 19:55:44 +0700

    Update test utils implementation

--- next-atom-with-auth/src/utils/testUtils.js
+++ next-atom-with-auth/src/utils/testUtils.js
@@ -1,12 +1,11 @@
 import { render as rtlRender } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { create as rtrCreate } from "react-test-renderer";
 import "@testing-library/jest-dom/extend-expect";
 import "jest-styled-components";
 
-const render = (component, options) => rtlRender(component, options);
+export const render = (component, options) => rtlRender(component, options);
 
-const create = component => rtrCreate(component);
+export const create = component => rtrCreate(component);
 
-export * from "@testing-library/react";
-
-export { render, create };
+export const fireEvent = userEvent;
