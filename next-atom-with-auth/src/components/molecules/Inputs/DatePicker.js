453b32913b03c0b3bbcee6834ceca41de76aec37
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-22 06:17:09 +0700

    Add styled system prop to DatePicker

--- next-atom-with-auth/src/components/molecules/Inputs/DatePicker.js
+++ next-atom-with-auth/src/components/molecules/Inputs/DatePicker.js
@@ -1,2 +1,18 @@
-import { DatePicker } from "antd";
+import { DatePicker as Picker } from "antd";
+import styled from "styled-components";
+import { border, layout, space } from "styled-system";
+import propTypes from "@styled-system/prop-types";
+
+const DatePicker = styled(Picker)`
+  ${border}
+  ${layout}
+  ${space}
+`;
+
+DatePicker.propTypes = {
+  ...propTypes.border,
+  ...propTypes.layout,
+  ...propTypes.space
+};
+
 export default DatePicker;
