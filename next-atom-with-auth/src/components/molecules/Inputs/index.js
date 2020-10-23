9974a152c17827c7c4ffc29840cef3c39629cbf2
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-10-23 19:37:37 +0700

    Index input components

---
+++ next-atom-with-auth/src/components/molecules/Inputs/index.js
@@ -0,0 +1,4 @@
+export { default as DatePicker } from "./DatePicker";
+export { default as InputImage } from "./Image";
+export { default as Select } from "./Select";
+export { default as InputText } from "./Text";
